2db2856165ce98d49fb2a82c935b9196b259d1eb
oumakavoula <camoy@hotmail.fr>
2016-03-08 17:50:26 +0100

    xx

--- question.php
+++ question.php
@@ -15,7 +15,12 @@
   <body>
   	<div class="container">
   		<div class="row">
-  			  <div class="input-field col s12">
+  			<div class="col s12 titre-entete">
+  				Question
+  			</div>
+  		</div>
+  		<div class="row">
+  			  <div class="input-field col s6">
 			    <select>
 			      <option value="" disabled selected>Choose your option</option>
 			      <option value="1">Option 1</option>
@@ -23,7 +28,13 @@
 			      <option value="3">Option 3</option>
 			    </select>
 			    <label>Materialize Select</label>
-			  </div>
+			  </div><!-- FIN COL S12 -->
+	  	</div><!-- FIN ROW -->
+	  	<div class="row">
+          	<div class="input-field col s6">
+	          <input id="last_name" type="text" class="validate">
+	          <label class="blanc" for="last_name">Last Name</label>
+	        </div>
   		</div><!-- ROW -->
   	</div><!-- FIN CONTAINER -->
 
@@ -31,7 +42,12 @@
 
   	<script type="text/javascript" src="jquery-1.12.0.min.js"></script>
   	<script type="text/javascript" src="style/materialize/js/materialize.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+  	<script type="text/javascript">
+  		$(document).ready(function() {
+    		$('select').material_select();
+		});
+  	</script>
 
   </body>
 </html>
