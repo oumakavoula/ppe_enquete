4827496f42667b25bc1261e0ebf76ee817498dd1
oumakavoula <camoy@hotmail.fr>
2016-03-08 16:13:31 +0100

    xaxaax

--- index.php
+++ index.php
@@ -9,11 +9,27 @@
 
 	<!-- Bootstrap -->
 	<link href="style/materialize/css/materialize.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style/style.css">
 
   </head>
   <body>
+  	<div class="container">
+  		<div class="row">
+  			<div id="xaxa" class="col s12">
+  			<input id="lol"></input>
+  			</div>
+
+  		</div><!-- ROW -->
+  	</div><!-- FIN CONTAINER -->
 
 
 
+  	<script type="text/javascript" src="style/materialize/js/materialize.min.js"></script>
+  	<script type="text/javascript" src="jquery-1.12.0.min.js"></script>
+  	<script type="text/javascript">
+  		$("#lol").keyup(function(){
+  			$('#xaxa').css("background-color","red");
+  		})
+  	</script>
   </body>
 </html>
